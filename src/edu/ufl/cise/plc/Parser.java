e80d98622195997c78f65601f3f7ae1b1ee2c9ce
yoonmgyg <yoonmgyg@gmail.com>
2022-02-19 17:37:27 -0500

    added conditional expression

--- src/edu/ufl/cise/plc/Parser.java
+++ src/edu/ufl/cise/plc/Parser.java
@@ -1,32 +1,126 @@
 package edu.ufl.cise.plc;
 
 import edu.ufl.cise.plc.Token;
+import edu.ufl.cise.plc.IToken.Kind;
+import edu.ufl.cise.plc.ast.*;
 import java.util.ArrayList;
 import java.util.List;
 
 
-class Parser implements IParser {
-  private final List<Token> tokens = new ArrayList<>();
-  private int current = 0;
-
-  Parser(List<Token> tokens) {
-    this.tokens = tokens;
-  }
+public class Parser implements IParser {
+	private final List<Token> tokens;
+	private int current = 0;
+	private IToken t;
   
+	Parser(List<Token> tokens) {
+		this.tokens = tokens;
+	}
   
-  private Expr equality() {
-	    Expr expr = comparison();
+	// Referenced from Parsing 4
+	protected boolean isKind(Kind kind) {
+		return t.getKind() == kind;
+	}
+  
+	protected boolean isKind(Kind... kinds) {
+		for (Kind k: kinds){
+			if (k == t.getKind()) {
+				return true;
+			}	
+		}
+		return false;
+	}
 
-	    while (match(BANG_EQUAL, EQUAL_EQUAL)) {
-	      Token operator = previous();
-	      Expr right = comparison();
-	      expr = new Expr.Binary(expr, operator, right);
-	    }
+	//Referenced from Crafting Interpreters 6.2
+	private Token peek() {
+		return tokens.get(current);
+	}
 
-	    return expr;
+	//Referenced from Crafting Interpreters 6.2
+	private Token previous() {
+		return tokens.get(current - 1);
+	}
+
+	//Referenced from Crafting Interpreters 6.2
+
+	private boolean isAtEnd() {
+		return peek().kind == Kind.EOF;
+	}
+
+	//Referenced from Crafting Interpreters 6.2
+	private Token consume() {
+		if (!isAtEnd()) current++;
+		t = tokens.get(current);
+		return previous();
+	}
+	
+	//expression
+	private Expr expr() {
+		Expr e = cond();
+		if (cond() != null) {
+			return e;
+		}
+		else {
+			e = or();
+			return e;
+		}
+	}
+	
+	
+	// conditional
+	private Expr cond() throws SyntaxException {
+		if (isKind(Kind.KW_IF)) {
+			consume();
+			if (isKind(Kind.LPAREN)) {
+				consume();
+				Expr e = null;
+				e = expr();
+				if (isKind(Kind.RPAREN)) {
+					e = expr();
+					if (isKind(Kind.KW_ELSE)) {
+						e = expr();
+						if (isKind(Kind.KW_FI)) {
+							return e;
+						}
+					}
+				}
+			}
+			throw new SyntaxException("Invalid conditional");
+		}
+		return null;
+	}
+
+	private Expr or() throws SyntaxException {
+	}
+	   /*
+		
+	  public Expr factor() {   
+		  IToken firstToken = t;
+		  Expr e = null;
+		  if (isKind(INT_LIT)){
+			  e = new IntLitExpr(firstToken);
+		      consume();
+		  } 
+		  else if (isKind(LPAREN))){
+			consume(); 
+		    e = expr(); 
+		    match(RPAREN); 
+		  }
+		  else error();
+		  return e;
 	  }
-  
-  private Expr expression() {
-	    return equality();
-  }
+	  
+	  private void term() {
+		  factor();
+		  while (isKind(Kind.TIMES, Kind.DIV)) {consume(); term();};
+	  }
+	  public void expr() {
+		  term();
+		  while(isKind(Kind.PLUS, Kind.MINUS)) {consume(); term();}
+	  }
+	    */
+	
+	@Override
+	public ASTNode parse() throws PLCException {\
+		return null;
+	}
 }
